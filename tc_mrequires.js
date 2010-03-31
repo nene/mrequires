4dfcaa299ad97998ff2c93987e4c7e7e6754c37b
Rene Saarsoo <nene@triin.net>
2010-03-31 14:25:53 +0300

    Using same() instead of equals() in QUnit tests.
    Currently it doesn't make a difference, but it's the better practice.

--- tc_mrequires.js
+++ tc_mrequires.js
@@ -4,22 +4,22 @@ test("makeUrl with only default path", function() {
   var makeUrl = mRequires.init({"": "foo/"}).makeUrl;
 
   // components are by default JavaScript files
-  equals(makeUrl("bar"), "foo/bar.js");
-  equals(makeUrl("bar.baz"), "foo/bar/baz.js");
-  equals(makeUrl("bar.baz"), "foo/bar/baz.js");
-  equals(makeUrl("bar.bar.baz"), "foo/bar/bar/baz.js");
+  same(makeUrl("bar"), "foo/bar.js");
+  same(makeUrl("bar.baz"), "foo/bar/baz.js");
+  same(makeUrl("bar.baz"), "foo/bar/baz.js");
+  same(makeUrl("bar.bar.baz"), "foo/bar/bar/baz.js");
   
   // .js extension is optional
-  equals(makeUrl("bar.js"), "foo/bar.js");
-  equals(makeUrl("bar.js.baz"), "foo/bar/js/baz.js");
+  same(makeUrl("bar.js"), "foo/bar.js");
+  same(makeUrl("bar.js.baz"), "foo/bar/js/baz.js");
 
   // .css extension is required for CSS files
-  equals(makeUrl("bar.css"), "foo/bar.css");
-  equals(makeUrl("bar.baz.css"), "foo/bar/baz.css");
+  same(makeUrl("bar.css"), "foo/bar.css");
+  same(makeUrl("bar.baz.css"), "foo/bar/baz.css");
   
   // UPPERCASE extensions are silly, and therefore not acceptable
-  equals(makeUrl("bar.JS"), "foo/bar/JS.js");
-  equals(makeUrl("bar.CSS"), "foo/bar/CSS.js");
+  same(makeUrl("bar.JS"), "foo/bar/JS.js");
+  same(makeUrl("bar.CSS"), "foo/bar/CSS.js");
 });
 
 test("makeUrl with paths for some namespaces", function() {
@@ -29,13 +29,13 @@ test("makeUrl with paths for some namespaces", function() {
     "": "default/"
   }).makeUrl;
 
-  equals(makeUrl("Foo.bar"), "foo/bar.js");
-  equals(makeUrl("Bar.baz"), "../lib/bar/baz.js");
-  equals(makeUrl("Hoo.bar"), "default/Hoo/bar.js");
-  equals(makeUrl("Foo.bar.css"), "foo/bar.css");
+  same(makeUrl("Foo.bar"), "foo/bar.js");
+  same(makeUrl("Bar.baz"), "../lib/bar/baz.js");
+  same(makeUrl("Hoo.bar"), "default/Hoo/bar.js");
+  same(makeUrl("Foo.bar.css"), "foo/bar.css");
   
   // ensure, that we don't use Foo namespace here
-  equals(makeUrl("Foobar"), "default/Foobar.js");
+  same(makeUrl("Foobar"), "default/Foobar.js");
 });
 
 
